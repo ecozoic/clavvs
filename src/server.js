28268c5db5b7d6f4573eec7adbb39dbe0ee7dd2a
ecozoic <johnnn.mitchell@gmail.com>
2016-04-14 20:59:46 -0400

    setup express server

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv-safe').load();
+
+var path = require('path');
+var express = require('express');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var serveStatic = require('serve-static');
+var errorHandler = require('errorhandler');
+
+var app = express();
+
+// configure views & view engine here...
+
+app.use(compression());
+app.use(favicon(path.resolve(__dirname, '../public/favicon.ico')));
+app.use(logger('dev'));
+
+// add method overrides, sessions, body parsers, multers here...
+
+app.use(serveStatic(path.resolve(__dirname, '../public')));
+
+// add routes here
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler());
+}
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(path.resolve(__dirname, '../public/404.html'));
+})
+
+app.listen(process.env.PORT, function() {
+  console.log('Server listening on port: ' + process.env.PORT);
+});
