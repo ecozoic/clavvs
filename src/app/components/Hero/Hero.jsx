ef90b829d542afa798bb118c33d157d8b2ebe210
ecozoic <johnnn.mitchell@gmail.com>
2018-05-24 11:43:05 -0400

    style hero

--- src/app/components/Hero/Hero.jsx
+++ src/app/components/Hero/Hero.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import HeroButton from '../HeroButton';
 
+import img from '../../../images/clavvs_single.png';
+
+const HeroImage = styled.section`
+  background: url('${img}') no-repeat center / cover;
+  border-bottom: 1px solid #639;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: flex-end;
+  min-height: 700px;
+  padding-bottom: 20px;
+`;
+
+const HeroButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 const Hero = () => (
-  <section>
-    <div>
+  <HeroImage>
+    <HeroButtonContainer>
       <HeroButton href="https://open.spotify.com/artist/2JcWOIfDeHESCg42COyDy1">
         Music
       </HeroButton>
       <HeroButton href="https://clavvs.bigcartel.com">
         Merch
       </HeroButton>
-    </div>
-  </section>
+    </HeroButtonContainer>
+  </HeroImage>
 );
 
 export default Hero;
