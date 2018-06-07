c5cb884a6c9d6baaabdad830f41a83b7c36b5dd0
ecozoic <johnnn.mitchell@gmail.com>
2018-06-07 10:34:45 -0400

    firebase auth working

--- src/app/components/admin/Admin/Admin.jsx
+++ src/app/components/admin/Admin/Admin.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import firebase from 'firebase/app';
 
 const Admin = () => (
-  <h2>Admin</h2>
+  <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
 );
 
 export default Admin;
--- src/app/components/admin/Login/Login.jsx
+++ src/app/components/admin/Login/Login.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from 'firebase/app';
 
-const Login = () => (
-  <h2>Login</h2>
-);
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleEmailInput = (event) => {
+    this.setState({
+      email: event.target.value,
+    });
+  }
+
+  handlePasswordInput = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  handleSubmit = () => {
+    firebase.auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="container">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-for */}
+            <label htmlFor="email" className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                id="email"
+                type="email"
+                onInput={this.handleEmailInput}
+              />
+            </div>
+          </div>
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-for */}
+            <label htmlFor="password" className="label">Password</label>
+            <div className="control">
+              <input
+                className="input"
+                id="password"
+                type="password"
+                onInput={this.handlePasswordInput}
+              />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button
+                className="button is-link"
+                type="button"
+                onClick={this.handleSubmit}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default Login;
